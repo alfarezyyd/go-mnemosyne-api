74c7908dde70a7f6773e939da21687f633a65bfe
alfarezyyd <adityaalfarezyd@gmail.com>
2024-12-28 14:30:58 +0700

    feat: initialize app

--- main.go
+++ main.go
@@ -1,21 +1,38 @@
 package main
 
 import (
-  "fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"go-mnemosyne-api/config"
 )
 
 //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Println("Hello and welcome, %s!", s)
+	viperConfig := viper.New()
+	viperConfig.SetConfigFile(".env")
+	viperConfig.AddConfigPath(".")
+	viperConfig.AutomaticEnv()
+	viperConfig.ReadInConfig()
 
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+	// Database Initialization
+	databaseCredentials := &config.DatabaseCredentials{
+		DatabaseHost:     viperConfig.GetString("DATABASE_HOST"),
+		DatabasePort:     viperConfig.GetString("DATABASE_PORT"),
+		DatabaseName:     viperConfig.GetString("DATABASE_NAME"),
+		DatabasePassword: viperConfig.GetString("DATABASE_PASSWORD"),
+		DatabaseUsername: viperConfig.GetString("DATABASE_USERNAME"),
+	}
+
+	databaseInstance := config.NewDatabaseConnection(databaseCredentials)
+	databaseInstance.GetDatabaseConnection()
+
+	// Gin Initialization
+	ginEngine := gin.Default()
+	ginEngine.Use(gin.Recovery())
+	ginError := ginEngine.Run()
+	if ginError != nil {
+		panic(ginError)
+	}
 }
