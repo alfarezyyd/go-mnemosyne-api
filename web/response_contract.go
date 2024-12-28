51429cfe9e0e02854f5afe1af786e3bb94faa98c
alfarezyyd <adityaalfarezyd@gmail.com>
2024-12-29 03:42:39 +0700

    feat: add func constructor

--- web/response_contract.go
+++ web/response_contract.go
@@ -1,10 +1,17 @@
 package web
 
-import "go-mnemosyne-api/exception"
-
 type ResponseContract struct {
-	Status  bool                   `json:"status"`
-	Message string                 `json:"message"`
-	Data    interface{}            `json:"data,omitempty"`
-	Error   *exception.ClientError `json:"error,omitempty"`
+	Status  bool        `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+	Error   interface{} `json:"error,omitempty"`
+}
+
+func NewResponseContract(statusResp bool, messageResp string, dataResp interface{}, Error interface{}) *ResponseContract {
+	return &ResponseContract{
+		Status:  statusResp,
+		Message: messageResp,
+		Data:    dataResp,
+		Error:   Error,
+	}
 }
