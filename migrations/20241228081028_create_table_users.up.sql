67b964cf22c1b959ae20a258c91082717f11dc49
alfarezyyd <adityaalfarezyd@gmail.com>
2024-12-28 22:37:06 +0700

    feat: update schema

--- migrations/20241228081028_create_table_users.up.sql
+++ migrations/20241228081028_create_table_users.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE users
 (
     id                  BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,                                  -- ID unik untuk setiap pengguna
-    name                VARCHAR(255)        NOT NULL,                                                -- Nama pengguna
-    email               VARCHAR(255) UNIQUE NOT NULL,                                                -- Email pengguna, harus unik
-    password            VARCHAR(255)        NOT NULL,                                                -- Hash kata sandi pengguna
+    name                VARCHAR(255) NOT NULL,                                                       -- Nama pengguna
+    email               VARCHAR(255) UNIQUE    DEFAULT NULL,                                         -- Email pengguna, harus unik
+    password            VARCHAR(255) NOT NULL,                                                       -- Hash kata sandi pengguna
     role                ENUM ('Admin', 'User') DEFAULT 'User',
     email_verified_at   TIMESTAMP              DEFAULT NULL,
-    phone_number        VARCHAR(15)            DEFAULT NULL,                                         -- Nomor telepon pengguna (opsional)
+    phone_number        VARCHAR(15) UNIQUE     DEFAULT NULL,                                         -- Nomor telepon pengguna (opsional)
     profile_picture     VARCHAR(255)           DEFAULT NULL,                                         -- URL foto profil pengguna (opsional)
     is_active           BOOLEAN                DEFAULT TRUE,                                         -- Status keaktifan pengguna
     language_preference VARCHAR(5)             DEFAULT 'id',
