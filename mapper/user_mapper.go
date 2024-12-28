ade6c34376b9c87c3e528c7a9455b12a45f24d1d
alfarezyyd <adityaalfarezyd@gmail.com>
2024-12-29 06:08:32 +0700

    feat: hash password

--- mapper/user_mapper.go
+++ mapper/user_mapper.go
@@ -6,12 +6,16 @@ import (
 	"go-mnemosyne-api/helper"
 	"go-mnemosyne-api/model"
 	"go-mnemosyne-api/user/dto"
+	"golang.org/x/crypto/bcrypt"
 	"net/http"
 )
 
 func MapUserDtoIntoUserModel[T *dto.CreateUserDto](userTransferObject T) *model.User {
 	var userModel model.User
 	err := mapstructure.Decode(userTransferObject, &userModel)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(userModel.Password), 14)
+	helper.CheckErrorOperation(err, exception.NewClientError(http.StatusBadRequest, exception.ErrBadRequest))
+	userModel.Password = string(hashedPassword)
 	helper.CheckErrorOperation(err, exception.NewClientError(http.StatusBadRequest, exception.ErrBadRequest))
 	return &userModel
 }
