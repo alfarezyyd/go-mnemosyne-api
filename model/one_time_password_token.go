872bf9e4c7c215b588d6c15d3cacbec83b5b465e
alfarezyyd <adityaalfarezyd@gmail.com>
2024-12-29 17:05:10 +0700

    fix: typo

--- model/one_time_password_token.go
+++ model/one_time_password_token.go
@@ -6,6 +6,6 @@ type OneTimePasswordToken struct {
 	ID          uint64    `gorm:"column:id;primary_key;auto_increment"`
 	UserId      uint64    `gorm:"column:user_id"`
 	User        *User     `gorm:"foreignKey:user_id"`
-	HashedToken string    `gorm:"column:hashed_Token"`
+	HashedToken string    `gorm:"column:hashed_token"`
 	ExpiresAt   time.Time `gorm:"column:expires_at"`
 }
