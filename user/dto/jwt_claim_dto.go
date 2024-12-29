7c6e57f8a3e712c6b8521737e7fbe8c5bc7ac6ae
alfarezyyd <adityaalfarezyd@gmail.com>
2024-12-30 05:01:37 +0700

    feat: change to pointer because can null

--- user/dto/jwt_claim_dto.go
+++ user/dto/jwt_claim_dto.go
@@ -1,6 +1,6 @@
 package dto
 
 type JwtClaimDto struct {
-	Email       string `json:"email" mapstructure:"email"`
-	PhoneNumber string `json:"phone_number" mapstructure:"phone_number"`
+	Email       *string `json:"email" mapstructure:"email"`
+	PhoneNumber *string `json:"phone_number" mapstructure:"phone_number"`
 }
