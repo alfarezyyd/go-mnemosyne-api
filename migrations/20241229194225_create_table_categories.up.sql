38d33a1b286ee43cd064eb58a6cab88018cf0c1f
alfarezyyd <adityaalfarezyd@gmail.com>
2024-12-30 02:46:20 +0700

    feat: add categories schema

---
+++ migrations/20241229194225_create_table_categories.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE categories
+(
+    id          BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,                      -- ID unik untuk setiap kategori
+    user_id     BIGINT UNSIGNED NOT NULL,                                        -- Relasi ke tabel `users`
+    name        VARCHAR(100)    NOT NULL,                                        -- Nama kategori
+    description TEXT      DEFAULT NULL,                                          -- Deskripsi kategori (opsional)
+    is_default  BOOLEAN   DEFAULT FALSE,                                         -- Menandai apakah kategori ini adalah kategori default
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,                             -- Waktu kategori dibuat
+    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- Waktu kategori diperbarui
+    FOREIGN KEY (user_id) REFERENCES users (id)                                  -- Relasi ke tabel `users`
+);
