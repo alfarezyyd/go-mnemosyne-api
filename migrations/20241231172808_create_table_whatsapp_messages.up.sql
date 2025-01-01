6e66a9da202fa2e794ad2de360a492b992395fa3
alfarezyyd <adityaalfarezyd@gmail.com>
2025-01-01 22:23:22 +0700

    feat: add for media message

--- migrations/20241231172808_create_table_whatsapp_messages.up.sql
+++ migrations/20241231172808_create_table_whatsapp_messages.up.sql
@@ -1,10 +1,13 @@
 CREATE TABLE whatsapp_messages
 (
-    id                VARCHAR(255) PRIMARY KEY,
-    name              VARCHAR(255) NOT NULL,
-    whatsapp_id        VARCHAR(255) NOT NULL,
+    id                  VARCHAR(255) PRIMARY KEY,
+    name                VARCHAR(255) NOT NULL,
+    whatsapp_id         VARCHAR(255) NOT NULL,
     sender_phone_number VARCHAR(255) NOT NULL,
-    timestamp         VARCHAR(255) NOT NULL,
-    type              VARCHAR(255) NOT NULL,
-    text              VARCHAR(255) NOT NULL
+    timestamp           VARCHAR(255) NOT NULL,
+    type                VARCHAR(255) NOT NULL,
+    text                VARCHAR(255),
+    mime_type           VARCHAR(255),
+    sha256              VARCHAR(255),
+    media_id            VARCHAR(255)
 )
